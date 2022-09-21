7ddf0bac36ddb3a38c8699ab608da63fb44f388b
shubham <shubham22121@gmail.com>
2022-09-21 23:09:05 +0530

    bookstore api

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bookstore/pkg/routes"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:3000", r))
+	fmt.Println("Server is running on port 3000")
+}
---
+++ pkg/routes/book-rotues.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"bookstore/pkg/controllers"
+
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/", controllers.Hello).Methods("GET")
+}
