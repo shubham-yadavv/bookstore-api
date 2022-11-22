31f7dda770461ed202c685d6ac19ee634572954f
shubham <shubham22121@gmail.com>
2022-11-22 21:19:31 +0530

    db connect

--- pkg/database/connection.go
+++ pkg/database/connection.go
@@ -22,7 +22,11 @@ func ConnectDB() {
 		log.Println("Idiot")
 	}
 
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		config.Config("DB_HOST"),
+		port, config.Config("DB_USER"),
+		config.Config("DB_PASSWORD"),
+		config.Config("DB_NAME"))
 
 	DB, err = gorm.Open(postgres.Open(dsn))
 
